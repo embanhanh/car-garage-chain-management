e5dfb7fa1c318dfed243afb948c1256126d6fb2e
zont09 <zontcn09@gmail.com>
2024-12-25 13:40:56 +0700

    update modal add employees

--- src/renderer/src/pages/Employee/add_employee.jsx
+++ src/renderer/src/pages/Employee/add_employee.jsx
@@ -2,10 +2,13 @@ import TextFieldForm from "../../components/text_field";
 import './add_employee.css'
 import DropdownForm from "../../components/dropdown.jsx";
 import { useState } from 'react'
+import SearchDropdown from "../../components/dropdown.jsx";
+import ComboBox from '../../components/Combobox.jsx'
 
 function AddEmployee() {
     const [conName, setConName] = useState("");
     const [conEmail, setConEmail] = useState("");
+    const [conSex, setConSex] = useState(0);
     return <div>
         <div className="z-ae-container w-100">
             <div className="z-ae-row-data row w-100">
@@ -17,7 +20,10 @@ function AddEmployee() {
                 <TextFieldForm className = "col-6" hintText="Nhập địa chỉ" name="Địa chỉ" onChange= {(value) => {setConEmail(value)}}></TextFieldForm>
             </div>
             <div className="z-ae-row-data row w-100">
-                <DropdownForm options={["Nam", "Nữ", "Bê đê"]} className = "col-6" hintText="Chọn giới tính" name="Giới tính" onChange= {(value) => {setConName(value)}}></DropdownForm>
+                <div className="col-6">
+                <ComboBox options={[{value: 0, label: "Nam"}, {value: 1, label: "Nữ"},{value: -1, label: "Bê đê"}]} className = "col-6" placeholder="Chọn giới tính" value={conSex} onChange= {(value) => {setConSex(value)}}></ComboBox>
+                </div>
+                
                 <TextFieldForm className = "col-6" hintText="Nhập số điện thoại" name="Số điện thoại" onChange= {(value) => {setConEmail(value)}}></TextFieldForm>
             </div>
             <div className="z-ae-row-data row w-100">
