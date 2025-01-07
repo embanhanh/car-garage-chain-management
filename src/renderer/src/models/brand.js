21ed1b65ac040358f02f2a5e80fe06b203a4bed7
thong134 <thongheo413@gmail.com>
2025-01-08 01:27:50 +0700

    Regulation

---
+++ src/renderer/src/models/brand.js
@@ -0,0 +1,27 @@
+export class Brand {
+    static relations = []
+
+    constructor({
+        id = '',
+        name = '',
+        model = '',
+        createdAt = new Date()
+    } = {}) {
+        this.id = id
+        this.name = name
+        this.model = model
+        this.createdAt = createdAt
+    }
+
+    toFirestore() {
+        return {
+            name: this.name,
+            model: this.model,
+            createdAt: this.createdAt
+        }
+    }
+
+    static fromFirestore(data) {
+        return new Brand(data)
+    }
+}
